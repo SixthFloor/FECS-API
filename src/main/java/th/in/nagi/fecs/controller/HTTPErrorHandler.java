b4c621efc8ed65d3572baa365960faab6d6643bf
Frank <thanachot7@gmail.com>
2015-10-25 22:43:14 +0700

    add HTTP error handler

---
+++ src/main/java/th/in/nagi/fecs/controller/HTTPErrorHandler.java
@@ -0,0 +1,32 @@
+package th.in.nagi.fecs.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import th.in.nagi.fecs.message.ErrorMessage;
+import th.in.nagi.fecs.message.Message;
+
+@RestController
+public class HTTPErrorHandler {
+
+	@ResponseBody
+	@RequestMapping(value = "/400")
+	public Message error400() {
+		return new ErrorMessage(Message.ERROR, "Bad Request");
+	}
+
+	@ResponseBody
+	@RequestMapping(value = "/404")
+	public Message error404() {
+		return new ErrorMessage(Message.ERROR, "Page Not Found");
+	}
+
+	@ResponseBody
+	@RequestMapping(value = "/500")
+	public Message error500() {
+		return new ErrorMessage(Message.ERROR, "Internal Server Error");
+	}
+
+}
