7f760ab651ea95d286e14b348cbc81994bb343a3
Chonnipa Kittisiriprasert <Nagisama.j@gmail.com>
2015-12-08 03:38:00 +0700

    httpclient post

--- src/main/java/th/in/nagi/fecs/controller/PaymentController.java
+++ src/main/java/th/in/nagi/fecs/controller/PaymentController.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.UnsupportedEncodingException;
 import java.net.SocketTimeoutException;
+import java.util.Scanner;
 
 import org.apache.commons.httpclient.ConnectTimeoutException;
 import org.apache.http.HttpResponse;
@@ -12,6 +13,8 @@ import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -60,14 +63,13 @@ public class PaymentController extends BaseController {
 	 */
 	@ResponseBody
 	@RequestMapping(value = "/validate", method = RequestMethod.POST)
-	public ResponseEntity<?> validate(
-			//			@RequestHeader(value = "Authorization") String token,
+	public ResponseEntity<?> validate(@RequestHeader(value = "Authorization") String token,
 			@RequestParam(value = "orderNumber", required = true) Integer orderNumber,
 			@RequestBody WebPayment webPayment) {
-		//		if (!authenticationService.checkPermission(token, authenticationService.STAFF, authenticationService.MANAGER,
-		//				authenticationService.OWNER)) {
-		//			return new ResponseEntity<Message>(new Message("This payment does not allow"), HttpStatus.FORBIDDEN);
-		//		}
+		if (!authenticationService.checkPermission(token, authenticationService.STAFF, authenticationService.MANAGER,
+				authenticationService.OWNER)) {
+			return new ResponseEntity<Message>(new Message("This payment does not allow"), HttpStatus.FORBIDDEN);
+		}
 
 		Order order = orderService.findByKey(orderNumber);
 		if (order == null) {
@@ -87,14 +89,13 @@ public class PaymentController extends BaseController {
 	 */
 	@ResponseBody
 	@RequestMapping(value = "/pay", method = RequestMethod.POST)
-	public ResponseEntity<?> pay(
-			//			@RequestHeader(value = "Authorization") String token,
+	public ResponseEntity<?> pay(@RequestHeader(value = "Authorization") String token,
 			@RequestParam(value = "orderNumber", required = true) Integer orderNumber,
 			@RequestBody WebPayment webPayment) {
-		//		if (!authenticationService.checkPermission(token, authenticationService.MEMBER, authenticationService.STAFF,
-		//				authenticationService.MANAGER, authenticationService.OWNER)) {
-		//			return new ResponseEntity<Message>(new Message("This payment does not allow"), HttpStatus.FORBIDDEN);
-		//		}
+		if (!authenticationService.checkPermission(token, authenticationService.MEMBER, authenticationService.STAFF,
+				authenticationService.MANAGER, authenticationService.OWNER)) {
+			return new ResponseEntity<Message>(new Message("This payment does not allow"), HttpStatus.FORBIDDEN);
+		}
 
 		Order order = orderService.findByKey(orderNumber);
 		if (order == null) {
@@ -102,6 +103,40 @@ public class PaymentController extends BaseController {
 					HttpStatus.BAD_REQUEST);
 		}
 
+		CloseableHttpClient httpClient = HttpClientBuilder.create().build();
+		String bank = "bank1";
+		String result = "";
+
+		try {
+			HttpPost request = new HttpPost("http://50.112.182.228:3000/" + bank + "/pay");
+			StringEntity params = new StringEntity("details={" + "\"card\": {" + "\"no\":\""
+					+ webPayment.getWebCreditCard().getNumber() + "\"," + "\"exp_date\":\""
+					+ webPayment.getWebCreditCard().getExpirationDate() + "\"}," + "\"cvv\":\"" + webPayment.getCvv()
+					+ "\"," + "\"price\":\"" + webPayment.getPrice() + "\"}");
+			request.addHeader("content-type", "application/x-www-form-urlencoded");
+			request.setEntity(params);
+			HttpResponse response = httpClient.execute(request);
+
+			JSONObject json = new JSONObject(response);
+			result = json.getString("result");
+
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+		} catch (ClientProtocolException e) {
+			e.printStackTrace();
+		} catch (SocketTimeoutException e) {
+			e.printStackTrace();
+		} catch (ConnectTimeoutException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (JSONException e) {
+			e.printStackTrace();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		//		if (result.equals("success")) {
 		Shipping slot;
 
 		try {
@@ -117,5 +152,8 @@ public class PaymentController extends BaseController {
 		}
 
 		return new ResponseEntity<Boolean>(true, HttpStatus.OK);
+		//		}
+
+		//		return new ResponseEntity<Message>(new Message("Invalid card information"), HttpStatus.BAD_REQUEST);
 	}
 }
